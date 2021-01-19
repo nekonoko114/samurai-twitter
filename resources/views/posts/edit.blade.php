a8118cf8a06bf2b3e9979addb7de67477c29df20
nekonoko114 <yoshua0826@gmail.com>
2021-01-19 23:34:11 +0900

    fix posts layoute

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,11 +1,26 @@
 @extends('layouts.layouts')
 
 @section('title','samurai_twitter')
+
+@section('copntent')
+
+<h1>Editing< Post/h1>
+
 <form method="POST"  action="/posts/{{ $post->id }}">
     {{ csrf_filed() }}
     <input type="hidden" name="_method" value="PUT">
-    <input type="text" name="title" value="{{ $post->title }}">
-    <input type="text" name="content" value="{{ $post->content }}">
-    <input type="submit">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Title</label>
+            <input type="text" class="form-control" aria-describedby="emailHelp" name="title" value="{{ $post->title }}">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Content</label>
+            <textarea class="form-control" name="content">{{ $post->content }}</textarea>
+        </div>
+        <button type="submit" class="btn btn-outline-primary">Submit</button>
 </form>
+
+<a href="/post/{{ $post->id }}">Show</a>
+<a href="/posts">Back</a>
+
 @endsection
